1aaed576f1ada47a047e6b0b59fe403d6108419f
khan2626 <israelkhan217@gmail.com>
2024-09-25 16:32:46 +0100

    blit objects on pygame surface

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import pygame
+import os
 
-width, height = 900, 400
-screen = pygame.display.set_mode((width, height))
+WIDTH, HEIGHT = 900, 400
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Spaceship Game')
+
+WHITE = (255, 255, 255)
+
+FPS = 60
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (55, 40)), 90)
+
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (55, 40)), 270)
+
+def draw_on_screen():
+    SCREEN.fill(WHITE)
+    SCREEN.blit(YELLOW_SPACESHIP, (300,100))
+    SCREEN.blit(RED_SPACESHIP, (700, 100))
+    pygame.display.update()
 
 def main():
+    clock = pygame.time.Clock()
     running = True
     while running:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
+        draw_on_screen()
+
     pygame.quit()
 
 if __name__ == '__main__':
