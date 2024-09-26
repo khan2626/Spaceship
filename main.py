a07da8d45fdf0d385411458f8f8eb4d11260fb89
khan2626 <israelkhan217@gmail.com>
2024-09-26 11:15:19 +0100

    added red spaceship moves

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Spaceship Game')
 
 WHITE = (255, 255, 255)
-BLACK = (0,0,0)
+BLACK = (0, 0, 0)
 
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 FPS = 60
 SPEED = 5
-BORDER = pygame.Rect(WIDTH/2, 0, 10, HEIGHT)
+BORDER = pygame.Rect(WIDTH/2 - 5, 0, 10, HEIGHT)
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
 YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
@@ -30,15 +30,24 @@ def draw_on_screen(yellow, red):
 def yellow_moves(keys_pressed, yellow):
 
     
-    if keys_pressed[pygame.K_a]: #left
+    if keys_pressed[pygame.K_a] and yellow.x - SPEED > 0: #left
         yellow.x -= SPEED
-    if keys_pressed[pygame.K_d]: #right
+    if keys_pressed[pygame.K_d] and yellow.x + SPEED + yellow.width < BORDER.x: #right
         yellow.x += SPEED
-    if keys_pressed[pygame.K_w]: #up
+    if keys_pressed[pygame.K_w] and yellow.y - SPEED > 0: #up
         yellow.y -= SPEED
-    if keys_pressed[pygame.K_s]: #down
+    if keys_pressed[pygame.K_s] and yellow.y + SPEED + yellow.height + 15 < HEIGHT: #down
         yellow.y += SPEED
 
+def red_moves(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT] and red.x - SPEED > BORDER.x + BORDER.width + 15: #left
+        red.x -= SPEED
+    if keys_pressed[pygame.K_RIGHT] and red.x + SPEED + red.width - 15 < WIDTH: #right
+        red.x += SPEED
+    if keys_pressed[pygame.K_UP] and red.y - SPEED > 0: #up
+        red.y -= SPEED
+    if keys_pressed[pygame.K_DOWN] and red.y + SPEED + red.height + 15< HEIGHT: #down
+        red.y += SPEED
 def main():
     yellow = pygame.Rect((100, 200), (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
     red = pygame.Rect(700, 200, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
@@ -55,6 +64,7 @@ def main():
         
         keys_pressed = pygame.key.get_pressed()
         yellow_moves(keys_pressed, yellow)
+        red_moves(keys_pressed, red)
         
 
     pygame.quit()
