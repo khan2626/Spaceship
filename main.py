fd14f133d8d3cbb924716cbe11ce79e67c6210c6
khan2626 <israelkhan217@gmail.com>
2024-09-26 09:27:30 +0100

    added yellow_spaceship moves

--- main.py
+++ main.py
@@ -6,22 +6,43 @@ SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Spaceship Game')
 
 WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 FPS = 60
+SPEED = 5
+BORDER = pygame.Rect(WIDTH/2, 0, 10, HEIGHT)
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
-YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (55, 40)), 90)
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 
 RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
-RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (55, 40)), 270)
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
-def draw_on_screen():
+def draw_on_screen(yellow, red):
     SCREEN.fill(WHITE)
-    SCREEN.blit(YELLOW_SPACESHIP, (300,100))
-    SCREEN.blit(RED_SPACESHIP, (700, 100))
+    pygame.draw.rect(SCREEN, BLACK, BORDER)
+    SCREEN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    SCREEN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
+def yellow_moves(keys_pressed, yellow):
+
+    
+    if keys_pressed[pygame.K_a]: #left
+        yellow.x -= SPEED
+    if keys_pressed[pygame.K_d]: #right
+        yellow.x += SPEED
+    if keys_pressed[pygame.K_w]: #up
+        yellow.y -= SPEED
+    if keys_pressed[pygame.K_s]: #down
+        yellow.y += SPEED
+
 def main():
+    yellow = pygame.Rect((100, 200), (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+    red = pygame.Rect(700, 200, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
     clock = pygame.time.Clock()
     running = True
     while running:
@@ -30,7 +51,11 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         
-        draw_on_screen()
+        draw_on_screen(yellow, red)
+        
+        keys_pressed = pygame.key.get_pressed()
+        yellow_moves(keys_pressed, yellow)
+        
 
     pygame.quit()
 
